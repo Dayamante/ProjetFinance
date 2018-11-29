1d2c4a55c9e4baee00b54dc496fc3da6007a13e2
Dayamante <antoine.gurnaud@outlook.fr>
2018-11-29 16:44:16 +0100

    login added

---
+++ login.js
@@ -0,0 +1,11 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "Esigelec_Groupe-6" && password == "123"){
+        alert ("Login successfully");
+        window.location = "action.html"; // Redirecting to other page.
+    return false;
+    }
+}
